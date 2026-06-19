949381304795b7d5eca2af78dd7316d9fa073797
Pranav Krishnakumar <pranavkumarpk720@gmail.com>
2026-06-19 19:56:29 +0400

    Wire domain/__init__.py re-exports for public API (M1)

---
+++ src/signal_copier/domain/__init__.py
@@ -0,0 +1,19 @@
+from signal_copier.domain.signal import (
+    FailureReason,
+    ParsedSignal,
+    ParseFailure,
+    ParseResult,
+    Signal,
+    derive_signal_id,
+    parse_signal,
+)
+
+__all__ = [
+    "FailureReason",
+    "ParseFailure",
+    "ParsedSignal",
+    "ParseResult",
+    "Signal",
+    "derive_signal_id",
+    "parse_signal",
+]
